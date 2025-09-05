bcc2673f8521ef56b180b9e89f7e49e2e91b9663
Ludwin <ludwinsaulromero@gmail.com>
2025-09-05 07:40:45 -0600

    Clases

--- Programa.py
+++ Programa.py
@@ -11,7 +11,11 @@ cantidad vendida.
 """
 
 #Clases
-
+class Producto:
+    def __init__(self, nombre, categoria, precio):
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio = precio
 
 #Funciones
 def menu():
